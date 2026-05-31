65fcbc1d0b81fdec0c1f66d850a4964cb57a8f25
Roman Furtel <roman@furtel.com.ua>
2026-05-31 16:25:32 +0300

    Update 00-PS-Allow.bat

--- 00-PS-Allow.bat
+++ 00-PS-Allow.bat
@@ -1,4 +1,3 @@
-xcopy "%~dp0*.*" "C:\temp\" /Y
 powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -Force"
 powershell.exe -Command "Get-ChildItem "C:\Temp" -Recurse | Unblock-File"
 pause
