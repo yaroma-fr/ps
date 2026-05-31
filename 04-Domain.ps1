d74b24d7428d9e8073cc1ccbef668d8919a3d09e
Roman Furtel <roman@furtel.com.ua>
2026-05-31 16:28:59 +0300

    Add files via upload

---
+++ 04-Domain.ps1
@@ -0,0 +1,8 @@
+﻿$NewName = [Microsoft.VisualBasic.Interaction]::InputBox("Введіть нову назву комп'ютера", "Перейменування", $env:COMPUTERNAME)
+
+if ($NewName -eq "") {
+    Write-Host "Назву не введено, скрипт зупинено" -ForegroundColor Red
+    exit 1
+}
+
+Add-Computer -DomainName "your.domain.com" -NewName $NewName -Credential (Get-Credential) -Restart
