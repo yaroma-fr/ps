696e7c70c6864b75ea564758f5e3ee2dceb61320
Roman Furtel <roman@furtel.com.ua>
2026-05-31 17:07:43 +0300

    Delete 04-Domain.ps1

--- 04-Domain.ps1
+++
@@ -1,8 +0,0 @@
-﻿$NewName = [Microsoft.VisualBasic.Interaction]::InputBox("Введіть нову назву комп'ютера", "Перейменування", $env:COMPUTERNAME)
-
-if ($NewName -eq "") {
-    Write-Host "Назву не введено, скрипт зупинено" -ForegroundColor Red
-    exit 1
-}
-
-Add-Computer -DomainName "your.domain.com" -NewName $NewName -Credential (Get-Credential) -Restart
