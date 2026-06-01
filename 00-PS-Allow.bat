37e6b92d61fcf20f77bceafdc6f09538964842ec
Roman Furtel <roman@furtel.com.ua>
2026-06-01 12:41:48 +0300

    Add files via upload

---
+++ 00-PS-Allow.bat
@@ -0,0 +1,11 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit
+)
+powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -Force"
+if not exist "C:\temp" mkdir "C:\temp"
+powershell.exe -Command "Get-ChildItem "C:\Temp" -Recurse | Unblock-File"
+powershell.exe -ExecutionPolicy Bypass -File "%~dp001-Power-AnyDesk.ps1"
+pause
